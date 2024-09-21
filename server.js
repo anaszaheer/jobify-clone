523e3ac94278da9ccd41a5ef16ff58074dc4bef6
Muhammad Anas <m.anas.zaheer@gmail.com>
2024-09-22 00:07:58 +0500

    changes

--- server.js
+++ server.js
@@ -29,12 +29,11 @@ cloudinary.config({
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, './public')));
-
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser());
 app.use(express.json());
 
@@ -44,7 +43,7 @@ app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 app.use('*', (req, res) => {
